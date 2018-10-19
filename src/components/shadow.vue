761aef09b27ce46a433fde8f4d056c3bc5e215e1
liu <31910599+BBBigCat@users.noreply.github.com>
2018-10-19 17:54:40 +0800

    做了一个遮罩组件，用了vue中的动画过度效果，在添加setTimeout方法时，this.$router.push的this指针会出问题，解决一，使用箭头函数，解决二，用变量that保存this

---
+++ src/components/shadow.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id='shadows'>
+    <div class="content">
+      <transition name='fade'>
+        <img alt="今日诗词" @click="changeHash()"  src="https://v2.jinrishici.com/one.svg?font-size=20&spacing=2" v-if="show">
+      </transition>
+      <br>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "shadows",
+  data() {
+    return {
+      show: true
+    };
+  },
+  methods: {
+    changeHash() {
+      this.show = false;
+      var that = this;
+      setTimeout(function() {
+        that.$router.push({
+          name: "Home"
+        });
+      }, 1000);
+    }
+  },
+};
+</script>
+<style scoped>
+#shadow {
+  position: relative;
+  width: 400px;
+  height: 400px;
+}
+
+.content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter,
+  .fade-leave-to
+
+  /* .fade-leave-active below version 2.1.8 */
+ {
+  opacity: 0;
+}
+</style>
